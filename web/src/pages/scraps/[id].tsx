f739ade6518167c303b8d6e5f76a3355cfed6c8e
MandeNaren <y2pgftpe@gmail.com>
2022-09-12 13:44:21 +0900

    detailページのデザイン

--- web/src/pages/scraps/[id].tsx
+++ web/src/pages/scraps/[id].tsx
@@ -15,10 +15,53 @@ import Typography from '@mui/material/Typography'
 
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
+import TextareaAutosize from '@mui/material/TextareaAutosize'
+
+import EditIcon from '@mui/icons-material/Edit'
+import CheckIcon from '@mui/icons-material/Check'
+import CloseIcon from '@mui/icons-material/Close'
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline'
+
+import { styled } from '@mui/material/styles'
+
+const CustomEditIcon = styled(EditIcon)({
+  padding: '8px',
+  color: 'gray',
+  fontSize: '30',
+  cursor: 'pointer',
+  transition: '0.15s',
+  borderRadius: '50%',
+  '&:hover': {
+    backgroundColor: 'rgba(128, 128, 128, 0.2)'
+  }
+})
+
+const CustomCheckIcon = styled(CheckIcon)({
+  padding: '8px',
+  color: 'green',
+  fontSize: '30',
+  cursor: 'pointer',
+  transition: '0.15s',
+  borderRadius: '50%',
+  '&:hover': {
+    backgroundColor: 'rgba(60, 179, 113, 0.2)'
+  }
+})
+
+const CustomCloseIcon = styled(CloseIcon)({
+  padding: '8px',
+  color: 'red',
+  fontSize: '30',
+  cursor: 'pointer',
+  transition: '0.15s',
+  borderRadius: '50%',
+  '&:hover': {
+    backgroundColor: 'rgba(255, 0, 0, 0.2)'
+  }
+})
 
 const DetailPage = () => {
   const router = useRouter()
-  console.log(router.query)
   const id = router.query.id as string
 
   // Scrapとそのcomment一覧のquery
@@ -26,8 +69,11 @@ const DetailPage = () => {
 
   const scrap = data?.scraps_by_pk!
 
-  // 編集のState
+  // 編集文字のState
   const [scrapEditInput, setScrapEditInput] = useState('')
+
+  // 編集状態かどうかのState
+  const [editActive, seteditActive] = useState(false)
 
   // scrap削除のmutation
   const [deleteMutate] = useDeleteScrapMutation({
@@ -62,17 +108,29 @@ const DetailPage = () => {
     }
   })
 
+  const handleEdit = (id: number, input: string) => {
+    editMutate({ variables: { id: id, title: input } })
+    seteditActive(false)
+  }
+
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error: {JSON.stringify(error)}</p>
 
   return (
     <>
       <Stack direction="row" spacing={2} sx={{ width: '80%', margin: '20px auto' }}>
-        <Typography sx={{ width: '80%', fontWeight: 'lighter' }}>{scrap.created_at}</Typography>
+        <Typography sx={{ display: 'flex', alignItems: 'center', width: '80%', fontWeight: 'lighter', color: 'gray' }}>
+          <Box sx={{ marginRight: '20px' }}>{scrap.created_at}</Box>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <ChatBubbleOutlineIcon />
+            {scrap.comments_aggregate.aggregate?.count}
+          </Box>
+        </Typography>
+
         <Button
           color="error"
           variant="contained"
-          sx={{ width: '180px' }}
+          sx={{ width: '200px' }}
           onClick={() => {
             deleteMutate({ variables: { id: scrap.id } })
           }}
@@ -81,32 +139,31 @@ const DetailPage = () => {
         </Button>
       </Stack>
 
-      <Typography variant="h4" sx={{ width: '80%', margin: '0 auto' }}>
-        {scrap.title}
-      </Typography>
-
-      <Stack direction="row" spacing={2} sx={{ width: '80%', margin: '20px auto' }}>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault()
-            // ScarpUpdateのmutation実行
-            editMutate({ variables: { id: scrap.id, title: scrapEditInput } })
-          }}
-        >
-          <TextField value={scrapEditInput} onChange={(e) => setScrapEditInput(e.target.value)} />
-          <Button type="submit" color="success" variant="contained" sx={{ width: '180px' }}>
-            スクラップを編集する
-          </Button>
-        </form>
-      </Stack>
+      {editActive ? (
+        <Stack direction="row" spacing={2} sx={{ width: '80%', margin: '20px auto' }}>
+          <TextField
+            variant="standard"
+            placeholder={scrap.title}
+            value={scrapEditInput}
+            onChange={(e) => setScrapEditInput(e.target.value)}
+          />
+          <CustomCheckIcon onClick={() => handleEdit(scrap.id, scrapEditInput)} />
+          <CustomCloseIcon onClick={() => seteditActive(false)} />
+        </Stack>
+      ) : (
+        <Stack direction="row" alignItems="center" sx={{ width: '80%', margin: '20px auto' }}>
+          <Typography variant="h4">{scrap.title}</Typography>
+          <CustomEditIcon onClick={() => seteditActive(true)} />
+        </Stack>
+      )}
 
       <Stack mt={3} rowGap={2} alignItems="center">
         {scrap.comments.map((comment) => {
           return (
             <Card sx={{ width: '80%' }}>
               <CardContent>
-                {comment.created_at}
-                <Typography variant="h6">{comment.body}</Typography>
+                <Typography sx={{ color: 'gray', marginBottom: '20px' }}>{comment.created_at}</Typography>
+                <Typography>{comment.body}</Typography>
               </CardContent>
             </Card>
           )
@@ -122,18 +179,25 @@ const DetailPage = () => {
           }}
         >
           <Stack spacing={2}>
-            <TextField
-              id="standard-multiline-static"
-              label="スクラップにコメントを追加"
-              variant="standard"
-              multiline
-              rows={4}
+            <TextareaAutosize
+              minRows={6}
+              placeholder="スクラップにコメントを追加"
+              style={{
+                border: 'none',
+                borderBottom: 'solid gray 1px',
+                outline: 'none'
+              }}
               value={commentInput}
               onChange={(e) => setCommentInput(e.target.value)}
             />
           </Stack>
           <Stack mt={4} spacing={2}>
-            <Button type="submit" variant="contained" sx={{ width: '180px', marginLeft: 'auto' }}>
+            <Button
+              type="submit"
+              variant="contained"
+              sx={{ width: '100px', marginLeft: 'auto' }}
+              disabled={!commentInput}
+            >
               投稿する
             </Button>
           </Stack>
